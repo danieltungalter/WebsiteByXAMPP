a87fd1a02dcee0483b96539b28ed20a2ce9f7c27
danieltungalter <36910582+danieltungalter@users.noreply.github.com>
2019-03-13 18:10:51 +0800

    Update home.php

--- htdocs/home.php
+++ htdocs/home.php
@@ -303,13 +303,13 @@ function showSlides(n) {
 
 <script type="text/javascript">
 $(function() {
-    /* 按下GoTop按鈕時的事件 */
+    /* Event happen when the GoTop button is pressed */
     $('#gotop').click(function(){
-        $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
+        $('html,body').animate({ scrollTop: 0 }, 'slow');   /* Return to the top */
         return false;
     });
 
-    /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
+    /* When the roll silde down over 400px, the GoTop button will appear */
     $(window).scroll(function() {
         if ( $(this).scrollTop() > 400){
             $('#gotop').fadeIn();
